ed2c2180533f0d0a1b6fe81381580f8da98e4e8d
quintydj <quintyjansen@gmail.com>
2023-07-25 13:55:11 +0200

    website afgemaakt

--- index.js
+++ index.js
@@ -2,27 +2,17 @@ const shareKnop = document.querySelector(".shareKnop");
 const verstopt2 = document.querySelector(".verstopt2");
 const info2 = document.querySelector(".info2");
 const shareVerstopt = document.querySelector(".shareVerstopt");
+const desktop = window.matchMedia("(min-width: 1440px)");
 
 window.onload = latenZien();
 
 shareKnop.addEventListener("click", latenZien);
+shareVerstopt.addEventListener("click", latenZien);
 
 function latenZien() {
   if (verstopt2.style.display === "none") {
     verstopt2.style.display = "block";
-    info2.style.display = "none";
   } else {
     verstopt2.style.display = "none";
-  }
-}
-
-shareVerstopt.addEventListener("click", verstoppen);
-
-function verstoppen() {
-  if (info2.style.display === "none") {
-    info2.style.display = "block";
-    verstopt2.style.display = "none";
-  } else {
-    info2.style.display = "none";
   }
 }
